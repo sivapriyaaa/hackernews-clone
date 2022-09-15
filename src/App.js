c28fb5756bb9042965d22e2acfb0000a70e38cf4
sivapriyaaa <sivapriyaap293@gmail.com>
2022-09-15 16:41:21 +0530

    navbar-update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,40 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div></div>
+      <div className="navbar">
+        <ul>
+          <li>
+            <a href="#hacker">
+              <strong>Hacker News </strong>
+            </a>
+          </li>
+
+          <li>
+            <a href="#home">new</a>
+          </li>
+          <li>
+            <a href="#news">past</a>
+          </li>
+          <li>
+            <a href="#contact">comments</a>
+          </li>
+          <li>
+            <a href="#about">ask</a>
+          </li>
+          <li>
+            <a href="#news">jobs</a>
+          </li>
+          <li>
+            <a href="#contact">show</a>
+          </li>
+          <li>
+            <a href="#about">submit</a>
+          </li>
+          <div className="login">
+            <a href="#login">login</a>
+          </div>
+        </ul>
+      </div>
     </div>
   );
 }
